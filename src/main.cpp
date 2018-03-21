3752ed810b729a2c86a87a79a6e8638b7aec16c3
IIIDimaIII <33569757+IIIDimaIII@users.noreply.github.com>
2018-03-21 16:13:16 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -267,7 +267,12 @@ int main() {
 				double current_delta_s = abs(check_car_s - car_s);
 				double pred_delta_s = abs((check_car_s - car_s) + (check_speed  - car_speed) * 1.); //in one sec
 				if ((current_delta_s < 15) or (pred_delta_s < 15)){
-					cout<< "caution!" << endl;
+					if(d - car_d < -2.0) {
+						cout<< "caution left!" << endl;
+					}
+					else if(d - car_d > 2.0) {
+						cout<< "caution right!" << endl;
+					}
 				}
 
 				if (d < (2+4*lane+2) && d > (2+4*lane-2)){
